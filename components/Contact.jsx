7c6fde02015631fb4a36831e0ac109fd6a58db60
Souvik606 <mukherjeesouvik606@gmail.com>
2025-05-10 03:31:49 +0530

    Fixed contact form design issue for mobile

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -4,8 +4,8 @@ import contactBg from '/contactbg.png'
 
 export default function Contact({ setActiveSection }) {
   return (
-    <section id="contact" className="py-20 px-6 bg-gray-900">
-      <div className="max-w-7xl px-6 mx-auto">
+    <section id="contact" className="py-20 px-2 bg-gray-900">
+      <div className="max-w-7xl mx-auto">
         {/* Header Section */}
         <div className="text-center mb-16">
           <h2 className="text-3xl md:text-4xl font-bold mb-4">
